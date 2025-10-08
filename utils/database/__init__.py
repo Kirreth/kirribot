82593b98fb9127e5f655eedc1e6bf860e93ecaaf
Kirreth <kirreth@googlemail.com>
2025-10-08 09:42:23 +0200

    add users.py import

--- utils/database/__init__.py
+++ utils/database/__init__.py
@@ -2,3 +2,6 @@ from .connection import setup_database
 from .users import *
 from .commands import *
 from .messages import *
+from .leveling import *
+from .bumps import *
+from .moderation import *
