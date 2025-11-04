04c0ed745b91f8e11d15193919fe0475ad232a72
Kirreth <kirreth@googlemail.com>
2025-11-04 10:15:23 +0100

    create function to safe prefix for each server/guild

--- utils/database/moderation.py
+++ utils/database/moderation.py
@@ -110,34 +110,3 @@ def get_bans(user_id: str, guild_id: str) -> List[Tuple[Any, ...]]:
         conn.close()
     return results
 
-# -------------------------------
-# Sanktionen-Channel
-# -------------------------------
-
-def get_sanctions_channel(guild_id: str) -> Optional[str]:
-    """Gibt die gespeicherte Sanctions-Channel-ID zurück"""
-    conn = get_connection()
-    cur = conn.cursor()
-    try:
-        cur.execute("SELECT sanctions_channel FROM server_status WHERE guild_id = %s", (guild_id,))
-        result = cur.fetchone()
-        return result[0] if result and result[0] else None
-    finally:
-        cur.close()
-        conn.close()
-
-def set_sanctions_channel(guild_id: str, channel_id: Optional[str]):
-    """Speichert die Sanctions-Channel-ID in der DB"""
-    conn = get_connection()
-    cur = conn.cursor()
-    try:
-        cur.execute("""
-            INSERT INTO guilds (guild_id, sanctions_channel)
-            VALUES (%s, %s)
-            ON DUPLICATE KEY UPDATE
-                sanctions_channel = VALUES(sanctions_channel)
-        """, (guild_id, channel_id))
-        conn.commit()
-    finally:
-        cur.close()
-        conn.close()
